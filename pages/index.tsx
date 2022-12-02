7156492b4f6193009e9516f7e6535d311d0afca1
Ben <swanzeyb2001@gmail.com>
2022-12-02 11:45:10 -0800

    Initial Fonts & Assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,9 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Ben Swanzey</title>
         <link rel="icon" href="/favicon.ico" />
+        <style> @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500&family=Raleway:wght@400;600;700&display=swap'); </style>
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      white: '#FFFFFF',
+      richBlack: '#000027',
+      seaGreen: '#00FFBD',
+      brightTurquoise: '#00E7DB',
+      lightSilver: '#D9D9D9',
+    },
+    fontFamily: {
+      'Raleway': ['Raleway', 'sans-serif'],
+      'Jakarta': ['Plus Jakarta Sans', 'sans-serif'],
+    },
   },
   plugins: [],
 }
