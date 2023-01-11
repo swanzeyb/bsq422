9156eda7704e3964bea0570a51e7a3424ea72407
Ben <swanzeyb2001@gmail.com>
2023-01-10 21:24:18 -0800

    Events api endpoint

---
+++ pages/api/strapi/event.ts
@@ -0,0 +1,13 @@
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+) {
+  try {
+    res.status(200).send(JSON.stringify(req?.query, null, 2)))
+  } catch (e) {
+    res.status(400).send('Bad Request')
+  }
+}
